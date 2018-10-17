60ee66f05532733f94806eeb4c8928a00ff2fc46
natashakaur <40043979+natashakaur@users.noreply.github.com>
2018-10-16 17:10:46 -0700

    Cool hotkeys added

---
+++ HotKeys.py
@@ -0,0 +1,30 @@
+
+
+# Mac Guide
+    Command ⌘
+    Shift ⇧
+    Option ⌥
+    Control ⌃
+    Caps Lock ⇪
+    fn
+
+# General Mac HotKeys
+    # hide all windows except for active window
+    option + cmd + H
+
+    # forward delete (i.e. delete next character)
+    Ctrl + D
+
+    # Show Desktop
+    fn + F11
+
+    # Open recently closed document
+    Cmd + Shift + T
+
+
+# Sublime Hotkeys (PC)
+    # switch projects
+    Ctrl + Alt + P
+
+    # open command pallete to see packages (i.e. floobits)
+    Ctrl + Shift + P
